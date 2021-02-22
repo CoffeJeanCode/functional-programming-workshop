380ebe8272a617c0de29f2902911ef2684d66614
Jean Pierre Ortiz Murcia <ortizmurciajeanpierre@gmail.com>
2021-02-22 15:28:49 -0500

    delete: build folder

--- build/dist/index.js
+++
@@ -1,6 +0,0 @@
-import Reveal from '../_snowpack/pkg/revealjs.js';
-import Hightlight from '../_snowpack/pkg/revealjs/plugin/highlight/highlight.js';
-
-Reveal.initialize({
-  plugins: [Hightlight],
-});
